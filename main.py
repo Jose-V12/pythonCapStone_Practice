b71f30ab0d5dff32d95625410350fdfa5e14d556
Jose-V12 <jv123144@gmail.com>
2022-12-13 09:56:51 -0600

    created function to execute queries

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import mysql.connector
 from mysql.connector import Error
-def create_server_connection(host_name, user_name, user_password):
+def create_server_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
+            database=db_name
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -20,6 +21,12 @@ def create_database(connection, query):
         print("Database created successfully")
     except Error as err:
         print(f"Error: '{err}'")
-connection = create_server_connection("localhost", "root", "student")
-create_database_query = "CREATE DATABASE hospital"
-create_database(connection, create_database_query)
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+connection = create_server_connection("localhost", "root", "student", "hospital")
